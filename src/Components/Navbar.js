8bd3d3bd980390fce725a8cba71b861798b14f6f
Aniket Kumar <76562459+aniket-24@users.noreply.github.com>
2023-06-07 00:38:19 +0530

    Update app name

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
     return (
       <div style={{ display: "flex", padding: "0.5" }}>
         <Link to="/" style={{ textDecoration: "none" }}>
-          <h1 style={{ marginTop: "1rem", marginLeft: "1rem" }}>Movies App</h1>
+          <h1 style={{ marginTop: "1rem", marginLeft: "1rem" }}>Movies Mania</h1>
         </Link>
         <Link to="/favourites" style={{ textDecoration: "none" }}>
           <h2 style={{ marginLeft: "2rem", marginTop: "1.5rem" }}>
